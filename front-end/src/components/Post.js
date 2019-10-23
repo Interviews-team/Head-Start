13162629489e119182e8a15c3550115046d99255
ahmadghzawi <ahmd.ghzawi@gmail.com>
2019-10-23 22:47:42 +0300

    test post component

--- front-end/src/components/Post.js
+++ front-end/src/components/Post.js
@@ -5,12 +5,24 @@ export default class Post extends Component {
   state = {};
 
   render() {
-    // if (this.props.users !== undefined) {
-    let { question, answer, field, user_id, page, post_id, post, comments, userRole } = this.props;
-    console.log('PROPS: ',this.props)
+    console.log("POST");
+    let {
+      question,
+      answer,
+      field,
+      user_id,
+      page,
+      post_id,
+      post,
+      comments,
+      userRole
+    } = this.props;
     if (page === "LandingPage") {
       return (
-        <Link to={{pathname: '/PostPage', state:{post_id}} } style={{textDecoration: 'none'}}>
+        <Link
+          to={{ pathname: "/PostPage", state: { post_id } }}
+          style={{ textDecoration: "none" }}
+        >
           <div>
             <h3>{question}</h3>
             <h4>{answer}</h4>
@@ -23,7 +35,10 @@ export default class Post extends Component {
 
     if (page === "HrQuestionsPage") {
       return (
-        <Link to={{pathname: '/PostPage', state:{post_id}} } style={{textDecoration: 'none'}}>
+        <Link
+          to={{ pathname: "/PostPage", state: { post_id } }}
+          style={{ textDecoration: "none" }}
+        >
           <div>
             <h3>{question}</h3>
             <h4>{answer}</h4>
@@ -35,71 +50,69 @@ export default class Post extends Component {
       );
     }
 
-   
-      if (page === "TechnicalQuestionsPage" && userRole === "Admin") {
-        return (
-          <div>
-            <h1>Post Component</h1>
-            <h3>Post_id:</h3>
-            {post._id}
-            <br />
-            <h3>Post_Question:</h3>
-            {post.question}
-            <br />
-            <h3>Post_answer:</h3>
-            {post.answer}
-            <br />
-            <h3>Post_field:</h3>
-            {post.field}
-            <br />
-            <h3>Post_user_id:</h3>
-            {post.user_id}
-            <br />
-            <Link
-              to={{
-                pathname: "/PostPage",
-                state: {
-                  id: post._id,
-                  question: post.question,
-                  answer: post.answer,
-                  field: post.field,
-                  user_id: post.user_id,
-                  user: userRole,
-                  comments: comments
-                }
-              }}
-            >
-              View Post
-            </Link>
-          </div>
-        );
-      } else {
-        return (
-          <div>
-            <h1>Post Component</h1>
-            <h3>Post_Question:</h3>
-            {post.question}
-            <br />
-            <h3>Post_field:</h3>
-            {post.field}
-            <br />
-            <Link
-              to={{
-                pathname: "/PostPage",
-                state: {
-                  id: post._id,
-                  question: post.question,
-                  user: userRole,
-                  comments: comments
-                }
-              }}
-            >
-              View Post
-            </Link>
-          </div>
-        );
-      }
-    // }
+    if (page === "TechnicalQuestionsPage" && userRole === "Admin") {
+      return (
+        <div>
+          <h1>Post Component</h1>
+          <h3>Post_id:</h3>
+          {post._id}
+          <br />
+          <h3>Post_Question:</h3>
+          {post.question}
+          <br />
+          <h3>Post_answer:</h3>
+          {post.answer}
+          <br />
+          <h3>Post_field:</h3>
+          {post.field}
+          <br />
+          <h3>Post_user_id:</h3>
+          {post.user_id}
+          <br />
+          <Link
+            to={{
+              pathname: "/PostPage",
+              state: {
+                id: post._id,
+                question: post.question,
+                answer: post.answer,
+                field: post.field,
+                user_id: post.user_id,
+                user: userRole,
+                comments: comments
+              }
+            }}
+          >
+            View Post
+          </Link>
+        </div>
+      );
+    } else if(userRole === 'user'){
+      return (
+        <div>
+          <h1>Post Component</h1>
+          <h3>Post_Question:</h3>
+          {post.question}
+          <br />
+          <h3>Post_field:</h3>
+          {post.field}
+          <br />
+          <Link
+            to={{
+              pathname: "/PostPage",
+              state: {
+                id: post._id,
+                question: post.question,
+                user: userRole,
+                comments: comments
+              }
+            }}
+          >
+            View Post
+          </Link>
+        </div>
+      );
+    }
     return null
   }
 }
